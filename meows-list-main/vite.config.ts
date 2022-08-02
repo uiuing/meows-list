7b063e08a6e304735e6f145a2516dca48f81142f
uiuing <uiuing@foxmail.com>
2022-08-03 00:43:11 +0800

    chore: Configuration and code specification
    - eslint
    - git commit hook

--- meows-list-main/vite.config.ts
+++ meows-list-main/vite.config.ts
@@ -1,13 +1,19 @@
+/* eslint-disable import/no-unresolved */
+
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
-import eslintPlugin from 'vite-plugin-eslint'
 
 export default defineConfig({
-  plugins: [vue(), eslintPlugin()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, '/src')
-    }
-  }
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ]
 })
